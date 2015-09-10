db24b41c5176883fba0b85af2b9312639e6e7bd8
gary.liu <gary.liu@logicmonitor.com>
2015-09-10 17:41:33 +0800

    ignoredSubForms_in_serializeForm

--- b2.js
+++ b2.js
@@ -312,11 +312,18 @@
     // 2. we support to define a value2 to specify the value when the checkbox is not checked, default is false
     serializeForm: function (formEl, ignorePrefix, needArray, ignoredParentClass) {
       var that = this;
-      var $paramEls = $(formEl || this.el).find('input, select, textarea')
+      var formEl = formEl || this.el;
+      var $paramEls = $(formEl).find('input, select, textarea')
         .filter(function () {
+            var notInIgnoredForm = false;
             var $parent = $(this).closest('.' + ignoredParentClass);
+            if ($parent.length === 0) {
+              notInIgnoredForm = true;
+            } else if (!$.contains(formEl, $parent.get(0))) {
+              notInIgnoredForm = true;
+            }
             // if the name of a element has a "ignore" prefix, it means not need to be serialized.
-            return $parent.length === 0 && this.name && this.name.indexOf(ignorePrefix || 'ignore') === -1;
+            return notInIgnoredForm && this.name && this.name.indexOf(ignorePrefix || 'ignore') === -1;
         });
 
       var params = {};
